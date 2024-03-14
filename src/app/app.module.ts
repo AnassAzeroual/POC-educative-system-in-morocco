dc0b5fbddf7db3cb28ea503dcf1e7dd7053106dc
Anass Azeroual <azeroual.anass2015@gmail.com>
2024-03-15 00:40:39 +0100

    try

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,6 @@ import { RouterModule, Routes } from "@angular/router";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { NotifierModule } from "angular-notifier";
 import { CookieService } from "ngx-cookie-service";
-import * as $ from 'jquery';
-import { FullCalendarModule } from "ng-fullcalendar";
 import { NeedAuthGuard } from "./NeedAuthGuard";
 import { AppComponent } from "./app.component";
 import { AccueilComponent } from "./components/accueil/accueil.component";
@@ -139,7 +137,6 @@ const appRoutes: Routes = [
     BrowserModule,
     HttpClientModule,
     NotifierModule,
-    FullCalendarModule,
     // FullCalendarModule,
     RouterModule.forRoot(
       appRoutes,
