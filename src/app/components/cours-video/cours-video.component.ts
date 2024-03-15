3e7c5d46a65624942a6b1decd2f290356e9b7231
Anass Azeroual <azeroual.anass2015@gmail.com>
2024-03-15 01:21:39 +0100

    Angular standalone

--- src/app/components/cours-video/cours-video.component.ts
+++ src/app/components/cours-video/cours-video.component.ts
@@ -9,120 +9,120 @@ import { BoxSidebarComponent } from '../box-sidebar/box-sidebar.component';
 import { BoutonHomeComponent } from '../ui/bouton-home/bouton-home.component';
 import { NavbarComponent } from '../ui/navbar/navbar.component';
 
-declare var $:any;
+declare var $: any;
 @Component({
-    selector: 'app-cours-video',
-    templateUrl: './cours-video.component.html',
-    styleUrls: ['./cours-video.component.css'],
-    standalone: true,
-    imports: [NavbarComponent, BoutonHomeComponent, BoxSidebarComponent, FooterComponent]
+  selector: 'app-cours-video',
+  templateUrl: './cours-video.component.html',
+  styleUrls: ['./cours-video.component.css'],
+  standalone: true,
+  imports: [NavbarComponent, BoutonHomeComponent, BoxSidebarComponent, FooterComponent]
 })
-export class CoursVideoComponent implements OnInit,AfterViewInit {
-  
+export class CoursVideoComponent implements OnInit, AfterViewInit {
+
   coursVideosTitle: string;
   questions: string[];
   video: string;
   videoFunctions: string;
-  @ViewChild('iframe') iframe:ElementRef;
+  @ViewChild('iframe') iframe: ElementRef;
 
   constructor(public sanitizer: DomSanitizer,
     private http: HttpClient,
     private router: Router,
     private route: ActivatedRoute,
-    private localStorageService:LocalStorageService,
-    private domSanitizer : DomSanitizer,
-    private scoringService:ScoringService) { 
-      this.video = "https://www.youtube.com/embed/HndV87XpkWg"
-      this.video +="?enablejsapi=1&version=3&playerapiid=ytplayer";
+    private localStorageService: LocalStorageService,
+    private domSanitizer: DomSanitizer,
+    private scoringService: ScoringService) {
+    this.video = "https://www.youtube.com/embed/HndV87XpkWg"
+    this.video += "?enablejsapi=1&version=3&playerapiid=ytplayer";
   }
 
-   //parameters
-   chemin;
-   blocCode=[];
+  //parameters
+  chemin;
+  blocCode = [];
 
-   ngAfterViewInit() {
-    this.iframe.nativeElement.style.height=this.iframe.nativeElement.contentWindow.document.body.scrollHeight+20 + 'px';
+  ngAfterViewInit() {
+    this.iframe.nativeElement.style.height = this.iframe.nativeElement.contentWindow.document.body.scrollHeight + 20 + 'px';
   }
 
-  showSubBloc(event){
-    let routefragment="";
-    for(let i=0;i<=this.blocCode.length-1;i++){
-      if(this.blocCode[i]["libelle_objet"]==event["libelle_objet"]){
-        routefragment=routefragment+this.blocCode[i]["libelle_objet"];
-        this.router.navigateByUrl('/blocs#'+routefragment);
+  showSubBloc(event) {
+    let routefragment = "";
+    for (let i = 0; i <= this.blocCode.length - 1; i++) {
+      if (this.blocCode[i]["libelle_objet"] == event["libelle_objet"]) {
+        routefragment = routefragment + this.blocCode[i]["libelle_objet"];
+        this.router.navigateByUrl('/blocs#' + routefragment);
         return false;
-      }else{
-          routefragment=routefragment+this.blocCode[i]["libelle_objet"]+'/';
+      } else {
+        routefragment = routefragment + this.blocCode[i]["libelle_objet"] + '/';
       }
     }
     //routefragment=routefragment+this.blocCode[this.blocCode.length-1];
   }
 
   ngOnInit() {
-    let local=this.localStorageService;
+    let local = this.localStorageService;
     let id;
     let name;
     //this.scoringService.updateScore("10","56");
 
-     this.route.fragment.subscribe(
-       (fragments) => {
-        let routesTable=fragments.split("/");
-        name=routesTable[0];
-        id=routesTable[routesTable.length-1];
+    this.route.fragment.subscribe(
+      (fragments) => {
+        let routesTable = fragments.split("/");
+        name = routesTable[0];
+        id = routesTable[routesTable.length - 1];
 
-        for(let i=0;i<routesTable.length-1;i++){
-          this.blocCode.push({"libelle_objet":routesTable[i]});
+        for (let i = 0; i < routesTable.length - 1; i++) {
+          this.blocCode.push({ "libelle_objet": routesTable[i] });
         }
       }
     );
 
     this.checkSession();
-    this.findexosById(local,name,id);
+    this.findexosById(local, name, id);
 
     $('#corrig_video').addClass('display--none');
     $('#exer_solution').addClass('display--none');
- 
+
   }
 
-    //function find cours
-  findexosById(local,name,id){
-      let chemin;let blocs;
-      let coursVideosTitle;
-      if(local.getLocalStorage(name)){
-        local.getLocalStorage(name).forEach(function(value,key){  
-          if(value["code_arbo"]==id){
-            chemin =value["chemin_arbo"];
-            if ( chemin == "" || chemin == null || chemin == undefined) {
-              console.log(chemin);
-              chemin="./assets/files/404-Page.html";
-            }else{
-              chemin = "https://www.youtube.com/embed/HndV87XpkWg";
-              chemin +="?enablejsapi=1&version=3&playerapiid=ytplayer";
-              coursVideosTitle=value["libelle_objet"];
-            }
+  //function find cours
+  findexosById(local, name, id) {
+    let chemin; let blocs;
+    let coursVideosTitle;
+    if (local.getLocalStorage(name)) {
+      local.getLocalStorage(name).forEach(function (value, key) {
+        if (value["code_arbo"] == id) {
+          chemin = value["chemin_arbo"];
+          if (chemin == "" || chemin == null || chemin == undefined) {
+            console.log(chemin);
+            chemin = "./assets/files/404-Page.html";
+          } else {
+            chemin = "https://www.youtube.com/embed/HndV87XpkWg";
+            chemin += "?enablejsapi=1&version=3&playerapiid=ytplayer";
+            coursVideosTitle = value["libelle_objet"];
           }
-        });
-        console.log(chemin)
-        this.chemin=this.domSanitizer.bypassSecurityTrustResourceUrl(chemin);
-        this.coursVideosTitle=coursVideosTitle;
-      }else{
-        this.localStorageService.destructSession();
-        this.router.navigateByUrl('/');
-      }
+        }
+      });
+      console.log(chemin)
+      this.chemin = this.domSanitizer.bypassSecurityTrustResourceUrl(chemin);
+      this.coursVideosTitle = coursVideosTitle;
+    } else {
+      this.localStorageService.destructSession();
+      this.router.navigateByUrl('/');
+    }
   }
 
-    //function check session
-    checkSession(){
-      this.http.post('../../../sessions/read.php', null)
-              .subscribe(
-                (res: any[]) => {
-                },
-                err => {
-                  this.localStorageService.destructSession();
-                  this.router.navigateByUrl('/');
-                }
-             );
-    }
+  //function check session
+  checkSession() {
+    this.http.post('../../../sessions/read.php', null)
+      .subscribe(
+        (res: any[]) => {
+        },
+        err => {
+          this.localStorageService.destructSession();
+          this.router.navigateByUrl('/');
+        }
+      );
+  }
 
 
 
