8adc9b02cce7d3502c9e17e68ecc447b15e1240d
Anass Azeroual <azeroual.anass2015@gmail.com>
2024-03-15 01:06:43 +0100

    Bootstrap the application using standalone APIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { NotifierModule } from 'angular-notifier';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: true,
+    imports: [RouterOutlet, NotifierModule]
 })
 export class AppComponent  implements OnInit {
 
